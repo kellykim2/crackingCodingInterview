5b2b160830256ee5f0df1a82f0095589eedfd4ea
Kelly <kelly.kim.222@my.csun.edu>
2019-04-15 10:12:51 -0700

    added xcode file extensions to gitignore

---
+++ CrackingCodingInterview/CrackingCodingInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrackingCodingInterview
+//
+//  Created by Kelly Kim on 4/15/19.
+//  Copyright © 2019 Kelly Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
