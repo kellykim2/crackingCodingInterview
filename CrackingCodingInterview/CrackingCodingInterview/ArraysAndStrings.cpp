dbab5de80ba2f0b869d33024883b075b1218a1d1
Kelly <kelly.kim.222@my.csun.edu>
2019-04-15 11:05:50 -0700

    Chapter 1 Problem #1 Non recursive solution

---
+++ CrackingCodingInterview/CrackingCodingInterview/ArraysAndStrings.cpp
@@ -0,0 +1,9 @@
+//
+//  ArraysAndStrings.cpp
+//  CrackingCodingInterview
+//
+//  Created by Kelly Kim on 4/15/19.
+//  Copyright © 2019 Kelly Kim. All rights reserved.
+//
+
+#include "ArraysAndStrings.hpp"
---
+++ CrackingCodingInterview/CrackingCodingInterview/ArraysAndStrings.hpp
@@ -0,0 +1,14 @@
+//
+//  ArraysAndStrings.hpp
+//  CrackingCodingInterview
+//
+//  Created by Kelly Kim on 4/15/19.
+//  Copyright © 2019 Kelly Kim. All rights reserved.
+//
+
+#ifndef ArraysAndStrings_hpp
+#define ArraysAndStrings_hpp
+
+#include <stdio.h>
+
+#endif /* ArraysAndStrings_hpp */
