db7a1a27f7371240f8ed0559fba43322abc27ce5
Kelly <kelly.kim.222@my.csun.edu>
2019-06-07 23:05:13 -0700

    3.7 solution

---
+++ CrackingCodingInterview/CrackingCodingInterview/SetOfStack.cpp
@@ -0,0 +1,9 @@
+//
+//  SetOfStack.cpp
+//  CrackingCodingInterview
+//
+//  Created by Kelly Kim on 6/7/19.
+//  Copyright © 2019 Kelly Kim. All rights reserved.
+//
+
+#include "SetOfStack.hpp"
---
+++ CrackingCodingInterview/CrackingCodingInterview/SetOfStack.hpp
@@ -0,0 +1,14 @@
+//
+//  SetOfStack.hpp
+//  CrackingCodingInterview
+//
+//  Created by Kelly Kim on 6/7/19.
+//  Copyright © 2019 Kelly Kim. All rights reserved.
+//
+
+#ifndef SetOfStack_hpp
+#define SetOfStack_hpp
+
+#include <stdio.h>
+
+#endif /* SetOfStack_hpp */
